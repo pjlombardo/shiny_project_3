69e16d7f9dd5e16e07aefd2038ad233bcc058177
pjlombardo <philip.j.lombardo@gmail.com>
2026-02-05 10:43:05 -0500

    Woops, forgot the ui file.

--- ui.R
+++ ui.R
@@ -3,6 +3,25 @@
 
 ui <- fluidPage(
   # Fill in your details here
+  h1("Interactive Heatmap for Mouse RNA sequence data."),
+  p(""),
+  br(),
+  column(12,
+    inputPanel(
+      numericInput("num_genes",
+                   "Choose the number of top genes shown in the plot.",
+                   value = 5,
+                   min = 1,
+                   max = 30),
+      selectInput("mouse_samples",
+                  "Select which mouse samples to include in the plot.",
+                  choices = list("sample 1" = "s1", "sample 2" = "s2"))
+    )
+  ),
+  br(),
+  column(12,
+    plotOutput("heatmap")
+  )
   
   
   
